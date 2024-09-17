60199c9cc741b1388e6b9b2ef84cabd30a4caa9d
Igor de Freitas Cruz <igor.freitas.cruz@icloud.com>
2024-09-17 17:15:07 -0300

    first commit

---
+++ function_app.py
@@ -0,0 +1,17 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp()
+
+
+@app.service_bus_queue_trigger(
+    arg_name="azservicebus",
+    queue_name="estoque",
+    connection="dental_SERVICEBUS",
+    is_session_enabled=True,
+)
+def servicebus_queue_trigger_update_estoque(azservicebus: func.ServiceBusMessage):
+    logging.info(
+        "Python ServiceBus Queue trigger processed a message: %s",
+        azservicebus.get_body().decode("utf-8"),
+    )
